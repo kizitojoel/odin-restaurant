6f19a13d8fefba3c270644488f71ea71f45da73e
Joel Kizito <kizitojoel16@gmail.com>
2023-07-24 16:02:57 +0300

    Code tab switcher onto website

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./style.css"
 import { generateHomepage } from "./homepage";
+import { generateMenupage } from "./menu";
 
 // Create the main content div and append it to the body
 const contentDiv = document.createElement("div");
@@ -15,20 +16,40 @@ const nav = document.createElement("nav");
 // Create the different tabs and add them to the nav bar
 const homeTab = document.createElement("div");
 homeTab.textContent = "Home";
-homeTab.classList.add("tab");
+homeTab.classList.add("tab", "tab-active");
+
 const menuTab = document.createElement("div");
 menuTab.textContent = "Menu"
 menuTab.classList.add("tab");
+menuTab.addEventListener('click', generateMenupage);
+
 const contactTab = document.createElement("div");
 contactTab.textContent = "Contact"
 contactTab.classList.add("tab");
 
 nav.append(homeTab, menuTab, contactTab);
-
-
 contentDiv.appendChild(nav);
 
-
 document.body.appendChild(contentDiv);
+
+// Function to clear content after a tab is clicked
+function clearContent(){
+    while (nav.nextElementSibling){
+        nav.nextElementSibling.remove();
+    }
+}
+
+// Add event listeners to the tabs
+const tabs = document.querySelectorAll(".tab");
+tabs.forEach(tab => {
+    tab.addEventListener('click', function() {
+        if (!tab.classList.contains("tab-active")){
+            tabs.forEach(tab => tab.classList.remove("tab-active"));
+            this.classList.add("tab-active");
+            clearContent();
+        }
+    })
+})
+
 
 document.addEventListener("DOMContentLoaded", generateHomepage);
